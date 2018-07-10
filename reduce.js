3db17c5290f0b9e49ad7f9dc8da799d4bad9a06c
yoni chanowitz <yonichanowitz@gmail.com>
2018-07-10 18:13:59 -0400

    Update and rename reduce to reduce.js

--- reduce.js
+++ reduce.js
@@ -26,6 +26,24 @@ returns single value
 
 execute once for each item in Array
 
+//if initialValue, previousValue should === initial value
+
+//if initialValue, currentValue === first value in array
+
+//if initialValue, callback starts i[0]
+
+//if !initialValue, previousValue should === first value in array
+
+//if !initialValue, currentValue === second value in array
+
+//if !initialValue, callback starts i[1]
+
+//if empty, no initialValue, throw TypeError
+
+//if one element, no initialValue, return only element without calling callback
+
+//if array empty, no initialValue, return initialValue
+
 first time called, accumulator and current can be one of two things
 1)initialvalue is something,
    accum equals initial,
