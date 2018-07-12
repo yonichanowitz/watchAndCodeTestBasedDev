ba9d27ca54ed5edc1f4f51e4c8feb72d4a80dc95
yoni chanowitz <yonichanowitz@gmail.com>
2018-07-12 16:09:01 -0400

    Update reduce.js

--- reduce.js
+++ reduce.js
@@ -37,27 +37,14 @@ execute once for each item in Array
 //if !initialValue, currentValue === second value in array
 //if !initialValue, callback starts i[1]
 
+edge cases
+
 //if empty, no initialValue, throw TypeError
 //if one element, no initialValue, return only element without calling callback
 //if array empty, no initialValue, return initialValue
 
-first time called, accumulator and current can be one of two things
-1)initialvalue is something,
-   accum equals initial,
-   current is first value in array.
-   callback starts at index 0
-2)no initial value,
-   accum equals first value in array,
-   current is the second
-   callback starts at index 1
+should actually reduce array
 
-edge cases
-
- if array is empty 'TypeError' is thrown
- 
- if only one value in array, and no initial value,
- or initial value is there but array is empty
-    item is returned without calling mapCallback
 
 example of edge cases
 reduce([hole,1],function(){})
