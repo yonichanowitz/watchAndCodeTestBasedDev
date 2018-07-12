8be24c79269d7c15fcd4b1e3db6ff107aec1bf63
yoni chanowitz <yonichanowitz@gmail.com>
2018-07-12 16:33:25 -0400

    Update reduce.js

--- reduce.js
+++ reduce.js
@@ -16,57 +16,32 @@ function reduce(origionalArray,callback,optionalThis){
 
 });
 
-/*
+
 fucntion reduce(array, current,  previous ){
 for(var i = 0; i < array.length;i++){
     
    }
  };
 
-returns single value
-
-execute once for each item in Array
-
-//should not run holes in array 
-//[hole,1]
-
-//. example
-//
-//
-
-//
-//
-//
-
-edge cases
-
-//
-//
-//
-
-//
-//
-//
-
-
-
-
-*/
 tests({
- 'if initialValue, previousValue should start with initial value';function(){
-  fail();  
-}
- 'if initialValue, currentValue === first value in array';function(){}
- 'if initialValue, callback starts i[0]';function(){}
- 'if !initialValue, previousValue should === first value in array';function(){}
- 'if !initialValue, currentValue === second value in array';function(){}
- 'if !initialValue, callback starts i[1]';function(){}
- 'if initialValue, array empty, return initialValue without callback';function(){}
- 'if no initialValue, one element, return only element without calling callback';function(){}
- 'if empty, no initialValue, throw TypeError';function(){}
- 'should exclude holes';function(){}
- 'should actually reduce array';function(){}
- 'should pass array as 4th arg to callback';function(){}
+ 'if initialValue, previousValue should start with initial value':function(){
+fail();
+
+},
+ 'if initialValue, currentValue === first value in array':function(){
+
+
+ },
+ 'if initialValue, callback starts i[0]':function(){},
+ 'if !initialValue, previousValue should === first value in array':function(){},
+ 'if !initialValue, currentValue === second value in array':function(){},
+ 'if !initialValue, callback starts i[1]':function(){},
+ 'if initialValue, array empty, return initialValue without callback':function(){},
+ 'if no initialValue, one element, return only element without calling callback':function(){},
+ 'if empty, no initialValue, throw TypeError':function(){},
+ 'should exclude holes':function(){},
+ 'should actually reduce array':function(){},
+ 'should pass array as 4th arg to callback':function(){}
       });
 
 </script>
