cd22fa17e8de9af05c30a6dd9781d2f68a67c2f6
yoni chanowitz <yonichanowitz@gmail.com>
2018-07-20 12:47:52 -0400

    Update reduce.js

--- reduce.js
+++ reduce.js
@@ -2,8 +2,9 @@
 
 <script>
 
+  
 function reduce(theArray,callback,initialValue){
-
+var valueToReturn = '';
 var startingIndex = 0;
 var resultsSoFar = initialValue;
 //if no initialValue
@@ -11,20 +12,23 @@ if(arguments.length < 3){
   resultsSoFar = theArray[startingIndex];
  startingIndex++;
 //if array has one element return it
-  if (Object.keys(theArray).length === 1){
-      var singleIndex = Object.keys(theArray)[0];
-      var singleElement = theArray[singleIndex];
-      return singleElement;
+//55:51
+if (Object.keys(theArray).length === 1){
+    var singleIndex = Object.keys(theArray)[0];
+    var singleElement = theArray[singleIndex];
+      return singleElement
+    }
+    if (Object.keys(theArray).length === 0){
+      return "TypeError";
+    }
   }else{
     //there is an initial value but nothing in array
     if(Object.keys(theArray).length === 0){
       return initialValue;
-    }
-  
-  }
-}
-  for(var i = startingIndex; i < theArray.length; i++){
-    callback(initialValue,theArray[i],i);
+      }
+    for(var i = startingIndex; i < theArray.length; i++){
+      callback(resultsSoFar,theArray[i],i);
+      }
     }
   };
 
@@ -81,22 +85,25 @@ tests({
      eq(currentIndex, 1);
    });
  },
- 'if initialValue, array empty, return initialValue without callback':function(){
+ 'if initialValue, array empty, return initialValue without calling callback':function(){
   var initialValueHere = 'zero';
   var numberOfTimesRun = 0;
    var reduceresult = reduce([,,,],function(){
    numberOfTimesRun++;
-    },'zero');
+ },initialValueHere);
     eq(reduceresult, initialValueHere);
     eq(numberOfTimesRun,0);
  },
  'if no initialValue, one element, return only element without calling callback':function(){
-
-    var reduceresult = reduce([,,2,],function(){ });
+   var numberOfTimesRun = 0;
+    var reduceresult = reduce([,,2,],function(){
+     numberOfTimesRun++;
+     });
      eq(reduceresult, 2);
  },
  'if empty, no initialValue, throw TypeError':function(){
-  fail();
+    var reduceresult = reduce([,,,,],function(){});
+    eq(reduceresult,"TypeError");
  },
  'should exclude holes':function(){
    fail();
@@ -109,7 +116,6 @@ tests({
  }
 
 });
-
 
 
 
