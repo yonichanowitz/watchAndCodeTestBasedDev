9f9c0b2310ff8025ca2624c239f5a5929db42f90
yoni chanowitz <yonichanowitz@gmail.com>
2018-07-31 15:59:09 -0400

    Create simpletest.js

---
+++ simpletest.js
@@ -0,0 +1,55 @@
+//editing and adding features to Gordon Zhu's TinyTest
+
+var TinyTest = {
+
+    run: function(tests) {
+        var failures = 0;
+        debugger;
+        for (var testName in tests) {
+            var testAction = tests[testName];
+            try {
+                testAction.apply(this);
+                console.log('Test:', testName, 'OK');
+            } catch (e) {
+                failures++;
+                console.error('Test:', testName, 'FAILED', e);
+                console.error(e.stack);
+            }
+        }
+        setTimeout(function() { // Give document a chance to complete
+            if (window.document && document.body) {
+                document.body.style.backgroundColor = (failures == 0 ? '#99ff99' : '#ff9999');
+            }
+        }, 0);
+    },
+
+    fail: function(msg) {
+        throw new Error('fail(): ' + msg);
+    },
+
+    assert: function(value, msg) {
+        if (!value) {
+            throw new Error('assert(): ' + msg);
+        }
+    },
+
+    assertEquals: function(expected, actual) {
+        if (expected != actual) {
+            throw new Error('assertEquals() "' + expected + '" != "' + actual + '"');
+        }
+    },
+
+    assertStrictEquals: function(expected, actual) {
+        if (expected !== actual) {
+            throw new Error('assertStrictEquals() "' + expected + '" !== "' + actual + '"');
+        }
+    },
+
+};
+
+var fail               = TinyTest.fail.bind(TinyTest),
+    assert             = TinyTest.assert.bind(TinyTest),
+    assertEquals       = TinyTest.assertEquals.bind(TinyTest),
+    eq                 = TinyTest.assertEquals.bind(TinyTest), // alias for assertEquals
+    assertStrictEquals = TinyTest.assertStrictEquals.bind(TinyTest),
+    tests              = TinyTest.run.bind(TinyTest);
